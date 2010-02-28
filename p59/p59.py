c32b9a070ade664d51abc94d51dd42eb3f84cc15
ghills <ghills@gmail.com>
2010-02-28 01:55:05 -0800

    reads the file and gets the data ready to be used in calculations

--- p59/p59.py
+++ p59/p59.py
@@ -5,5 +5,14 @@ print "Brute force attempt to break an xor cipher\n\n"
 
 f = open("cipher1.txt","r")
 
+data = f.read().split(",")
+# chomp off the newline
+data[-1] = data[-1].rstrip()
 f.close()
+
+# convert to list of ints instead of strings
+data = [ int(x) for x in data ]
+
+# we know the key is 3 lower case letters, so try every combination
+# and look at the first word to be in the english language
 
