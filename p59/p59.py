64bd4315e5466e9747f8689cb9e539af3cbd7fb7
ghills <ghills@gmail.com>
2010-02-28 01:44:23 -0800

    just getting started on p59 to brute force break a cipher encrypted text file

---
+++ p59/p59.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+print "Project Euler -- Problem 59"
+print "Brute force attempt to break an xor cipher\n\n"
+
+f = open("cipher1.txt","r")
+
+f.close()
+
--- template.py
+++ template.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python
 
+print "Project Euler -- Problem x"
 
