5807f37867ddbadf67547eaf63f163ff3bd8323f
ghills <ghills@gmail.com>
2010-03-02 00:10:34 -0800

    changed method for number of divisors to something found on stack overflow, still not getting right answers though.

--- p12/p12.py
+++ p12/p12.py
@@ -3,15 +3,37 @@
 print "Project Euler -- Problem 12"
 print "First triangle number with  over 500 divisors\n\n"
 
+import operator
+# A slightly efficient superset of primes.
+def PrimesPlus():
+	yield 2
+	yield 3
+	i = 5
+	while True:
+		yield i
+		if i % 6 == 1:
+			i += 2
+		i += 2
+
+# Returns a dict d with n = product p ^ d[p]
+def GetPrimeDecomp(n):
+	d = {}
+	primes = PrimesPlus()
+	for p in primes:
+		while n % p == 0:
+			n /= p
+			d[p] = d.setdefault(p, 0) + 1
+		if n == 1:
+			return d
+
 def num_divisors(n):
-	count = 0
-	for i in range(1,n+1):
-		if n % i == 0: count +=1
-	return count
+	d = GetPrimeDecomp(n)
+	powers_plus = map(lambda x: x+1, d.values())
+	return reduce(operator.mul, powers_plus, 1)
 
 n = 10
 
-while num_divisors(n*(n+1)/2) <= 500: n += 1
+while num_divisors(n*(n+1)/2) < 500: n += 1
 
 print "Solution: %d" % n
 
